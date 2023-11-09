12f83068b4f5ce7331774fc0c129f10f63864c6e
Gabriel Ccama <gabriel.ccama@tecsup.edu.pe>
2023-11-08 23:44:00 -0500

    laboratorio terminado

---
+++ app/Models/Cita.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cita extends Model
+{
+    use HasFactory;
+
+    public function Pacientes()
+    {
+        return $this->belongsTo(Paciente::class);
+    }
+
+    public function Diagnosticos()
+    {
+        return $this->belongsTo(Diagnostico::class);
+    }
+
+    public function Pagos()
+    {
+        return $this->hasOne(Pago::class);
+    }
+
+    public function Doctores()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+}
+
+
---
+++ app/Models/Doctor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    public function Especialidades()
+    {
+        return $this->belongsTo(Especialidad::class);
+    }
+
+    public function Citas()
+    {
+        return $this->hasMany(Cita::class);
+    }
+
+    public function Turnos()
+    {
+        return $this->hasMany(Turno::class);
+    }
+}
+
